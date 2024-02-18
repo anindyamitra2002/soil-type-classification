176c35718280899b32df5ff7dc949c7f3184223a
anindyamitra2002 <anindyamitra2018@gmail.com>
2024-02-18 20:27:30 +0530

    modified files

--- src/main.py
+++ src/main.py
@@ -1,2 +1,20 @@
-# Here is the user interface
-import gradio
+import gradio as gr
+import numpy as np
+import cv2
+from model import classify_image
+
+def main(img):
+    img = cv2.resize(img, (224,224))
+    img = img/255.0
+    img = np.expand_dims(img, axis=0)
+    label, accuracy = classify_image(img)
+    print(label)
+    out = {label: accuracy}
+    return out
+
+demo = gr.Interface(fn=main,
+             inputs=gr.Image(),
+             outputs=gr.Label(num_top_classes=1), allow_flagging='never')
+
+if __name__ == "__main__":
+    demo.launch()
--- src/model.py
+++ src/model.py
@@ -1,1 +1,19 @@
-# Here is the code of model load and prediction
+from keras.models import load_model
+import numpy as np
+
+labels = ['Red Soil', 'Clay Soil', 'Black Soil', 'Alluvial Soil', 'Slit SOil', 'Sand', 'Mary Soil', 'Chalky Soil']
+MODEL_PATH = r"D:\Official\Reseach Projects\Official Project\soil-type-classification\models\DenseNet121v2_95.h5"
+
+def load_CNN_model():
+    model = load_model(MODEL_PATH)
+    return model
+
+def classify_image(img):
+    model = load_CNN_model()
+    prediction = model.predict(img)
+    predicted_class = np.argmax(prediction)
+
+    predicted_label = labels[predicted_class]
+    accuracy = prediction[0][predicted_class]
+
+    return predicted_label, accuracy
